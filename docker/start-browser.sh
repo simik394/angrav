2dec43d2dd89d6a07a005ebd2b447245be305076
simik394 <simon.trousil@seznam.cz>
2025-12-14 04:47:03 +0100

    fix(docker): Fix volume permissions by running container as root with supervisord user drop

--- docker/start-browser.sh
+++ docker/start-browser.sh
@@ -7,9 +7,14 @@ set -e
 # Clean up stale X lock
 rm -f /tmp/.X99-lock
 
+
 echo "Starting Antigravity Browser Container..."
 echo "  VNC: port 5900"
 echo "  CDP: port 9223 (proxied)"
+
+# Fix permissions for angrav user (handles volume mount ownership issues)
+echo "Fixing permissions..."
+chown -R angrav:angrav /home/angrav
 
 # Start supervisord (runs as root to manage services)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
